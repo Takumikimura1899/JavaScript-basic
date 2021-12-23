a4651f97f5cb63f5b528ca3b0264f0dc30a41ffc
Takumikimura141899 <kimura141899@gmail.com>
2021-12-23 21:25:15 +0900

    [add] function

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+function logger() {
+  console.log('My name is taku');
+}
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :D');
+logger();
+logger();
+logger();
 
-// const interface = 'Audio'
-// const private = 123
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(typeof num, num);
