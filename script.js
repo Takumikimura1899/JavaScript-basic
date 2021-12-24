598c18aeac125de112ff7d637e6998439a7a6935
Takumikimura141899 <kimura141899@gmail.com>
2021-12-25 08:30:04 +0900

    [add] 関数チャレンジ

--- script.js
+++ script.js
@@ -80,3 +80,23 @@ const yearsUntilRetirement2 = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement2(1991, 'Jonas'));
 console.log(yearsUntilRetirement2(1950, 'Mike'));
+
+// challenge
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(300, 20);
+checkWinner(134, 596);
