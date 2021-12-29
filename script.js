e2981b16d5c0c6c003631fcd716f25b9325c5b68
Takumikimura141899 <kimura141899@gmail.com>
2021-12-29 16:41:11 +0900

    [add] 配列メソッド 追加　削除

--- script.js
+++ script.js
@@ -34,3 +34,28 @@ const age3 = calcAge(years[years.length - 1]);
 console.log(age1, age2, age3);
 
 console.log(years.map((year) => calcAge(year)));
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
