ecddee769d8b1f2232491139851e9401da76979c
Takumikimura141899 <kimura141899@gmail.com>
2021-12-30 20:49:49 +0900

    [add] object_this

--- script.js
+++ script.js
@@ -11,7 +11,60 @@ const jonasArray = [
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedmann',
-  age: 2037 - 1991,
+  birthYear: 1991,
   job: 'teacher',
   friends: ['Michel', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+  //   calcAge: (birthYear) => {
+  //     return 2037 - birthYear;
+  //   },
+  //   calcAge: function () {
+  //     return 2037 - this.birthYear;
+  //   },
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      jonas.job
+    }, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
+  },
 };
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt(
+//   'What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends'
+// );
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     ' Wrong request! Choose between firstName, lastName, age, job, and friends'
+//   );
+// }
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friends is called ${jonas.friends[0]}`
+);
+
+// console.log(jonas.calcAge(1991));
+// console.log(jonas['calcAge'](1991));
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
