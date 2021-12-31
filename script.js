6074f61f427c556d48f6360c5c43b3a2a7443949
Takumikimura141899 <kimura141899@gmail.com>
2021-12-31 10:06:05 +0900

    [add] for in for

--- script.js
+++ script.js
@@ -42,3 +42,15 @@ for (let i = 0; i < jonas.length; i++) {
 
   console.log(jonas[i], typeof jonas[i]);
 }
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------ Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
